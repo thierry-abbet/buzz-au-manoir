4941093e04a803cb540436e6c7bd6579d0f9bf28
thierry-abbet <sartyx@gmail.com>
2025-04-16 17:42:45 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,4 +12,5 @@ io.on('connection', socket => {
   console.log('User connected:', socket.id);
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
