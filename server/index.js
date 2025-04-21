7cdeb9f442edab370e1db562ad3d7c00e123ae53
thierry-abbet <sartyx@gmail.com>
2025-04-21 17:09:49 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
-const generateRoomName = require("./utils/names");
+const { generateRoomName } = require("./utils/names");
 
 const app = express();
 const server = http.createServer(app);
